b3b0b1e26745208de084f3b64b9f818e16f81dcd
Jamin Cheung <jamin.cheung@bloomingdales.com>
2019-11-16 22:33:17 -0500

    Fix webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,10 @@
 const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const common = require('./webpack.common.js');
 
 const config = merge.smartStrategy({ 'module.rules.use': 'prepend' })(common, {
   mode: 'production',
@@ -15,20 +15,22 @@ const config = merge.smartStrategy({ 'module.rules.use': 'prepend' })(common, {
   optimization: {
     minimizer: [
       new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        title: 'Jamin Cheung',
-        favicon: 'src/assets/images/jamin-cheung/jamin-cheung.jpg',
-        template: 'src/templates/index.hbs',
-        filename: '../index.html',
+      new TerserJSPlugin({
+        sourceMap: true,
       }),
-      new TerserJSPlugin({}),
       new OptimizeCSSAssetsPlugin({}),
     ],
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Jamin Cheung',
+      favicon: 'src/assets/images/jamin-cheung/jamin-cheung.jpg',
+      template: 'src/templates/index.hbs',
+      filename: '../index.html',
+    }),
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
-      chunkFilename: "[id].css"
+      chunkFilename: '[id].css',
     }),
   ],
   module: {
@@ -37,10 +39,10 @@ const config = merge.smartStrategy({ 'module.rules.use': 'prepend' })(common, {
         test: /\.(sa|sc|c)ss$/,
         use: [
           MiniCssExtractPlugin.loader,
-        ]
+        ],
       },
-    ]
-  }
+    ],
+  },
 });
 
 module.exports = config;
