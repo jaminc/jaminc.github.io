7b64cd0c985899cddfeea16205d48eff29116a33
Jamin Cheung <jamin.cheung@bloomingdales.com>
2019-11-17 13:05:20 -0500

    Move openState management to model

---
+++ src/js/state/Model.js
@@ -0,0 +1,31 @@
+class Model {
+  constructor(attributes = {}) {
+    this.attributes = attributes;
+
+    this.sidebarOpenStateCallbacks = [];
+  }
+
+  getState() {
+    return {
+      ...this.attributes,
+    };
+  }
+
+  updateSidebarOpenState(newState) {
+    const prevState = this.getState().isSidebarOpen;
+
+    if (newState !== prevState) {
+      this.attributes.isSidebarOpen = newState;
+
+      this.sidebarOpenStateCallbacks.forEach((cb) => cb(newState));
+    }
+  }
+
+  subscribeToSidebarOpenState(callback) {
+    if (typeof callback === 'function') {
+      this.sidebarOpenStateCallbacks.push(callback);
+    }
+  }
+}
+
+export default Model;
