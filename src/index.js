3ba1fe6a2bf7bb76e228b6464c3f43414ef31ec8
Jamin <jamincheung1@gmail.com>
2026-03-15 16:06:10 -0400

    Move all of vue setup to root directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import PageApp from './js/app';
-import './scss/main.scss';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const application = new PageApp();
-
-  application.start();
-});
--- src/js/constants.js
+++
@@ -1,8 +0,0 @@
-export const ESCAPE_KEY = 'Escape';
-export const TAB_KEY = 'Tab';
-
-export const MEDIUM_WIDTH_START = 800;
-
-// states
-export const SIDEBAR_OPEN_STATE = 'isSidebarOpen';
-export const SCROLLBAR_WIDTH = 'scrollbarWidth';
