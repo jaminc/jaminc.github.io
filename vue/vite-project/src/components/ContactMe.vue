89c233a1c2acb754cf53f5564e9a868a8451c291
Jamin <jamincheung1@gmail.com>
2026-03-15 15:24:41 -0400

    Add linkedin link

--- vue/vite-project/src/components/ContactMe.vue
+++ vue/vite-project/src/components/ContactMe.vue
@@ -1,7 +1,14 @@
 <template>
   <SectionTitle title="Contact Me" />
 
-  <img alt="github icon" src="@/assets/icons/github.svg" class="icon" />
+  <div class="contact-container">
+    <a href="http://www.github.com/jaminc">
+      <img alt="github icon" src="@/assets/icons/github.svg" class="icon" />
+    </a>
+    <a href="http://www.linkedin.com/in/jamincheung">
+      <img alt="github icon" src="@/assets/icons/linkedin.svg" class="icon" />
+    </a>
+  </div>
 </template>
 
 <script setup>
@@ -9,7 +16,12 @@ import SectionTitle from './SectionTitle.vue'
 </script>
 
 <style scoped>
+.contact-container {
+  display: flex;
+  gap: 1rem;
+}
+
 .icon {
-  width: 4rem;
+  width: 2rem;
 }
 </style>
