c342adfc1a650ceb120c62521ee5ba847f98023f
Jamin <jamincheung1@gmail.com>
2026-03-15 22:44:32 -0400

    Move content to separate component

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,12 @@
     <header>
       <h1>Jamin Cheung</h1>
     </header>
-    <div class="content">
-      <!-- <img
+    <!-- <img
     alt="jamin cheung profile picture"
     src="@/assets/images/jamin-cheung.jpg"
     class="profile-pic"
   /> -->
-
-      <section class="content-section">
-        <AboutMe />
-      </section>
-
-      <section class="content-section">
-        <WorkExperience />
-      </section>
-
-      <section class="content-section">
-        <ContactMe />
-      </section>
-    </div>
+    <MainContent />
 
     <ThemeSwitch />
   </div>
@@ -29,22 +16,20 @@
 
 <script setup>
 import ThemeSwitch from './components/ThemeSwitch.vue'
-import WorkExperience from './components/WorkExperience.vue'
-import ContactMe from './components/ContactMe.vue'
-import AboutMe from './components/AboutMe.vue'
+import MainContent from './components/MainContent.vue'
 </script>
 
 <style scoped>
 .container {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  margin: auto;
   padding: 2rem 4rem;
+  max-width: 1500px;
 }
 
 .profile-pic {
   border-radius: 50%;
-}
-
-.content-section {
-  padding: 2rem 0;
-  border-top: 1px solid var(--color-border);
 }
 </style>
