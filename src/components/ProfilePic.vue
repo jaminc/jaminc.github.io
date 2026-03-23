526e3637777afb7eb06b63b3f48fbc6b75ac0c97
Jamin <jamincheung1@gmail.com>
2026-03-22 21:05:13 -0400

    Use photo depending on dark mode

---
+++ src/components/ProfilePic.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <img
+      v-if="isDarkMode"
+      alt="Jamin Cheung profile picture"
+      src="@/assets/images/jamin-profile-pic-dark-mode.png"
+      class="profile-pic"
+    />
+    <img
+      v-else
+      alt="Jamin Cheung profile picture"
+      src="@/assets/images/jamin-profile-pic-light-mode.png"
+      class="profile-pic"
+    />
+  </div>
+</template>
+
+<script setup>
+import useDark from '@/use/useDark'
+
+const { isDarkMode } = useDark()
+</script>
+
+<style scoped>
+.profile-pic {
+  width: 6rem;
+  height: 6rem;
+  border-radius: 50%;
+}
+</style>
